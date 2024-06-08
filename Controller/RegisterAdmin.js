ed8dba53b2ed2a23bf365099f51d619f30863588
Eman Butt <ebutt940@gmail.com>
2024-06-08 11:52:57 +0500

    2nd update

--- Controller/RegisterAdmin.js
+++ Controller/RegisterAdmin.js
@@ -1,4 +1,3 @@
-
 import bcrypt from "bcrypt";
 import JWT from 'jsonwebtoken';
 import  registeradmin from '../Model/AdminSchema.js'
@@ -6,29 +5,24 @@ import  registeradmin from '../Model/AdminSchema.js'
 
 const register = async (req, res) => {
     try {
-        const { Email, Password } = req.body;
-
-        if ( !Email || !Password) {
-            res.send({
-                message: "Required Field"
-            });
-
-        }
-
+        const Email = 'ebutt@gmail.com';
+        const Password = '12345';
 
         const hash_password = await bcrypt.hash(Password, 10);
-        console.log(hash_password);
-
-        const saveuser = registeradmin({  Email, Password: hash_password }).save();
-        console.log(saveuser);
-        res.send({
-            message: 'User Registered'
-
-        });
+        console.log(hash_password); 
 
 
-
-
+        if ( Email === 'ebutt@gmail.com' && Password === '12345') {
+         const saveuser = registeradmin({  Email, Password: hash_password }).save();
+          res.send({
+            message: 'Login OK'
+          })
+        }
+        else{
+            res.send({
+                message: 'Login  not OK'
+              })
+        }
     } catch (error) {
         console.log(error)
         res.send({
@@ -37,29 +31,53 @@ const register = async (req, res) => {
     }
 
 }
-const login = async (req, res) => {
-    try {
-        const { Email, Password } = req.body;
-        const login = await registeradmin.findOne({ Email: Email})                // pehli Email database sa fetch kr rhai hn aur dosra emial hum uper user sa get kr rhai hn
-        console.log(login)
-        const token = JWT.sign({id:"_id"},`${process.env.PRIVATEKEY}`,{expiresIn:'1h'});
-        // console.log(token);
-        if (login) {
-            res.status(200).send({
-                message: "login OK"
-            })
-        }
-        else {
-            res.send({
-                message: "login not ok"
-            })
-        }}
-        catch (error) {
-            console.log(error)
-            res.send({
-                message: "Server error"
-            })
-        }
-    }
-
-    export {register , login};
+// const login = async (req, res) => {
+//     try {
+//         const { Email, Password } = req.body;
+//         const login = await registeradmin.findOne({ Email: Email})                // pehli Email database sa fetch kr rhai hn aur dosra emial hum uper user sa get kr rhai hn
+//         console.log(login)
+//         const token = JWT.sign({id:"_id"},`${process.env.PRIVATEKEY}`,{expiresIn:'1h'});
+//         // console.log(token);
+//         if (login) {
+//             res.status(200).send({
+//                 message: "login OK"
+//             })
+//         }
+//         else {
+//             res.send({
+//                 message: "login not ok"
+//             })
+//         }}
+//         catch (error) {
+//             console.log(error)
+//             res.send({
+//                 message: "Server error"
+//             })
+//         }
+//     }
+ 
+    const login = async (req, res) => {
+        try {
+            const { Email, Password } = req.body;
+            const login = await registeradmin.findOne({ Email: Email })                // pehli Email database sa fetch kr rhai hn aur dosra emial hum uper user sa get kr rhai hn
+            
+            const token = JWT.sign({id:"_id"},`${process.env.PRIVATEKEY}`,{expiresIn:'1h'});
+            console.log(token);
+            if (login) {
+                res.status(200).send({
+                    message: "login OK"
+                })
+            }
+            else {
+                res.send({
+                    message: "login not ok"
+                })
+            }}
+            catch (error) {
+                console.log(error)
+                res.send({
+                    message: "Server error"
+                })
+            }
+        } 
+    export {register , login };
